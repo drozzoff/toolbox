9bb437e1c984ab02ca2da3291b0b111476bd778b
Andrii Pastushenko <a.pastushenko@gsi.de>
2025-03-20 14:18:03 +0100

    Modified the separatrix finder to support different particle's species

--- toolbox/phase_space.py
+++ toolbox/phase_space.py
@@ -4,39 +4,42 @@ from tqdm.notebook import tqdm
 from xtrack import Line, Particles, ParticlesMonitor
 from pandas import DataFrame
 
-def _stable_particle_id(line: Line, particles: Particles, n_particles: int, **kwargs):
+def stable_particle_id(line: Line, particles: Particles, n_particles: int, **kwargs):
 	"""
 	Evaluates the `particle_id` of a particle in `particles` that is located
 	in a stable region.
-
+	
 	The function assumes `particles` are generated with a continous set of `x`.
-
+	
 	Unstable particles are at the beginning, stable particles at the end
 	"""
 	tw = line.twiss4d()
 	
+	if kwargs.get("debug", False):
+		import matplotlib.pyplot as plt
+		import seaborn as sns
+
 	line.track(
 		particles,
 		num_turns = kwargs.get("num_turns", 2000),
 		turn_by_turn_monitor = True
 	)
-	
 	rec = line.record_last_track
 	nc = tw.get_normalized_coordinates(rec.data)
-	
+
 	x_norms = np.split(nc.x_norm, n_particles)
 	px_norms = np.split(nc.px_norm, n_particles)
-
+	
 	Jx_mean = []
-
+	
 	particles.sort(interleave_lost_particles = True)
-
+	
 	for particle_id in range(n_particles):
 		lost_at_turn = particles.at_turn[particle_id]
 		Jx = list(map(lambda x, y: np.sqrt(x**2 + y**2), x_norms[particle_id][:lost_at_turn + 1], px_norms[particle_id][:lost_at_turn + 1]))
 	  
 		Jx_mean.append(np.mean(Jx))
-
+	
 	jx_diff = []
 	for i, var in enumerate(Jx_mean):
 		if i == 0:
@@ -46,12 +49,26 @@ def _stable_particle_id(line: Line, particles: Particles, n_particles: int, **kw
 			jx_diff.append(abs(var - Jx_mean[i - 1]))
 	
 	particle_id_stable = np.argmax(jx_diff)
+	if kwargs.get("debug", False):
+		sns.set_style("darkgrid")
+		fig, axes = plt.subplots()
+		for i in range(n_particles):
+			sns.scatterplot(
+				x = x_norms[i],
+				y = px_norms[i],
+				color = "red", 
+				alpha = 0.6, 
+				s = 5,
+				ax = axes
+			)    
+		
+		plt.show()
 	
 	return particle_id_stable, jx_diff
 
 
 
-def get_stable_limit(line: Line, test_range: List[float], ex_norm: float, n_particles: int, num_turns: int, precision = 1e-6, **kwargs):
+def get_stable_limit(line: Line, ion: dict, test_range: List[float], ex_norm: float, n_particles: int, num_turns: int, precision = 1e-6, **kwargs):
 	"""
 	Find a stable region of a given line.
 
@@ -79,49 +96,73 @@ def get_stable_limit(line: Line, test_range: List[float], ex_norm: float, n_part
 	with_progress = kwargs.get('with_progress', False)
 	
 	progress = tqdm(total = 100, desc = "Tracking to find a stable region", leave = True) if with_progress else None
-
+	
 	get_progress = lambda x: 1 - (x - precision) / x
 	progress_init = min(1, get_progress(test_range[1] - test_range[0])) * 100 # in %
 	
 	if with_progress:
 		progress.update(progress_init)
 
-	while (test_range[1] - test_range[0]) > precision:
+	iterations_data = {
+		'jx_diff': [],
+		'x_norm': []
+	}
+	iter_id = 0
+	while (test_range[1] - test_range[0]) > precision and iter_id < kwargs.get('max_iterations', 2000):
 		
 		x_test = np.linspace(test_range[0], test_range[1], n_particles)
+		iterations_data['x_norm'].append(x_test)
 		
-		test_particles = line.build_particles(
+		test_beam = line.build_particles(
 			x_norm = x_test,
-			px_norm = 0.0,
-			y_norm = 0.0,
-			py_norm = 0.0,
 			nemitt_x = ex_norm,
-			nemitt_y = 0.0,
+			mode = "normalized_transverse",
+			method = '4d'
+		)
+
+		test_particles = Particles(
+			mass0 = line.particle_ref.mass0, 
+			q0 = line.particle_ref.q0,
+			gamma0 = line.particle_ref.gamma0,
+			mass_ratio = ion['mass'] / line.particle_ref.mass0,
+			charge_ratio = ion['charge'] / line.particle_ref.q0,
+			x = test_beam.x,
+			px = test_beam.px,
 			zeta = 0.0,
 			delta = 0.0,
 			method = '4d'
 		)
-		
-		particle_id_stable, __ = _stable_particle_id(
+		particle_id_stable, jx_diff = stable_particle_id(
 			line = line, 
 			particles = test_particles, 
 			n_particles = n_particles,
-			num_turns = num_turns
+			num_turns = num_turns,
+			debug = kwargs.get("debug", False)
 		)
+		iterations_data['jx_diff'].append(jx_diff)
 		
-		test_range[0] = x_test[particle_id_stable - 1]
-		test_range[1] = x_test[particle_id_stable]         
-
+		shrinkage_strength = kwargs.get("shrinkage_strength", "max")
+		if shrinkage_strength == "max":
+			test_range[0] = x_test[particle_id_stable - 1]
+			test_range[1] = x_test[particle_id_stable]
+		elif isinstance(shrinkage_strength, int):
+			left = particle_id_stable - 1 - shrinkage_strength
+			right = particle_id_stable + shrinkage_strength
+			test_range[0] = x_test[left if left >= 0 else 0]
+			test_range[1] = x_test[right if right < n_particles else n_particles - 1]
+		
 		if with_progress:
 			progress_current = min(1, get_progress(test_range[1] - test_range[0])) * 100 # in %
 			progress.update(progress_current - progress_init) 
-
+	
 		progress_init = progress_current
-		
+
+		iter_id += 1
+	
 	if with_progress:
 		progress.close()
 		
-	return test_range
+	return test_range, iterations_data
 
 def get_phase_portrait2d(monitor: ParticlesMonitor, particles: Particles, at_turn: int, plane: str = 'x') -> DataFrame:
 	"""
