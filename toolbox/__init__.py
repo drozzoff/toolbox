7e5803d00054d507e087715c01649cba7886ebbe
Andrii Pastushenko <drozz.off@gmail.com>
2026-02-06 17:31:55 +0100

    added signal dummy signal generator

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,7 +1,7 @@
 from toolbox.apertures import realign_mad_apertures
 from toolbox.plotting import PlotContext, phase_space_evolution
 from toolbox.phase_space import *
-from toolbox.dashboard import ExtractionDashboard
+from toolbox.dashboard import ExtractionDashboard, DummyGenerator
 from toolbox.tune import *
 from toolbox.beamlines import exc_freq_chirp, _remove_inactive_multipoles_fix
 from toolbox.multigpu import track_multigpu
