451c0f665db47a233f19408651485e639422d1aa
Andrii Pastushenko <a.pastushenko@gsi.de>
2026-07-29 15:39:19 +0200

    Added a callback for tracking

--- toolbox/phase_space/extra.py
+++ toolbox/phase_space/extra.py
@@ -3,170 +3,102 @@ import xtrack as xt
 from pandas import DataFrame
 
 
-def get_phase_portrait2d(monitor: xt.ParticlesMonitor, particles: xt.Particles, at_turn: int, plane: str = 'x') -> DataFrame:
+class PhaseSpaceSampler:
 	"""
-	Get the phase space portrait of the particles at a given turn from a last track monitor (`.record_last_track`).
-	If the particle was lost before `at_turn` returns the last recorded coordinate.
-
-	Using custom `ParticlesMonitor` will fail, because it does have a structured data, eg. `monitor.x`.
-
-	Parameters
-	----------
-	particles
-		beam that was used for the tracking
-	at_turn
-		turn to get the phase space portrait at
-	plane
-		plane for the phase space portrait. Must be either 'x', 'y' or, 'z'
-
-	Returns
-	-------
-	DataFrame
-		phase space portrait
-	"""
-	particles.sort(interleave_lost_particles = True)
-
-	# if particle is lost before `at_turn` returns the turn it was lost at.
-	# Otherwise returns `at_turn`
-	at_turn_flat = list(map(lambda x: at_turn if x > at_turn else x, particles.at_turn))
+	The idea is that we reduce the resolution of the phase space. For instance to 64x64 or 100x100.
+	It is not practically possible to keep the phase space information for any turn when number of particles
+	used and number of turns are large. 
+	**E.g.** Tracking on GPU of 250k particles for 1kk turns means to store
+	**8 bytes x 2.5e4 x 1e6 ~ 2.0TB** of data in RAM. No computer would be able to do that.
 	
-	res = {}
-	
-	if plane == 'x':
-		res['x'] = np.array(list(map(lambda x, y: x[y], monitor.x, at_turn_flat)))
-		res['px'] = np.array(list(map(lambda x, y: x[y], monitor.px, at_turn_flat)))
-	elif plane == 'y':
-		res['y'] = np.array(list(map(lambda x, y: x[y], monitor.y, at_turn_flat)))
-		res['py'] = np.array(list(map(lambda x, y: x[y], monitor.py, at_turn_flat)))
-	elif plane == 'z':
-		res['zeta'] = np.array(list(map(lambda x, y: x[y], monitor.zeta, at_turn_flat)))
-		res['delta'] = np.array(list(map(lambda x, y: x[y], monitor.delta, at_turn_flat)))
-	else:
-		raise ValueError("plane must be either 'x', 'y' or 'z'")
 
-	return DataFrame(res)
+	But phase space scalled down to a resolution of 100x100 in selected window and recorded every 1k turns means
+	we only need **4 bytes  1000 x 100 x 100 ~ 40 MB** in `np.uint32` format.
 
-def get_phase_portrait4d(monitor: xt.ParticlesMonitor, particles: xt.Particles, at_turn: int) -> DataFrame:
+	The way is to use it as a callback during the tracking.
+
 	"""
-	Get the transverse phase space portrait of the particles at a given turn from a last track monitor (`.record_last_track`).
-	If the particle was lost before `at_turn` returns the last recorded coordinate.
-
-	Using custom `ParticlesMonitor` will fail, because it does have a structured data, eg. `monitor.x`.
-
-	Parameters
-	----------
-	particles
-		beam that was used for the tracking
-	at_turn
-		turn to get the phase space portrait at
-
-	Returns
-	-------
-	DataFrame
-		phase space portrait
-	"""
-	particles.sort(interleave_lost_particles = True)
-
-	# if particle is lost before `at_turn` returns the turn it was lost at.
-	# Otherwise returns `at_turn`
-	at_turn_flat = list(map(lambda x: at_turn if x > at_turn else x, particles.at_turn))
-	
-	res = {}
-	
-	res['x'] = np.array(list(map(lambda x, y: x[y], monitor.x, at_turn_flat)))
-	res['px'] = np.array(list(map(lambda x, y: x[y], monitor.px, at_turn_flat)))
-	res['y'] = np.array(list(map(lambda x, y: x[y], monitor.y, at_turn_flat)))
-	res['py'] = np.array(list(map(lambda x, y: x[y], monitor.py, at_turn_flat)))
-
-	return DataFrame(res)
-
-def get_phase_portrait6d(monitor: xt.ParticlesMonitor, particles: xt.Particles, at_turn: int) -> DataFrame:
-	"""
-	Get the transverse phase space portrait of the particles at a given turn from a last track monitor (`.record_last_track`).
-	If the particle was lost before `at_turn` returns the last recorded coordinate.
-
-	Using custom `ParticlesMonitor` will fail, because it does have a structured data, eg. `monitor.x`.
-
-	Parameters
-	----------
-	particles
-		beam that was used for the tracking
-	at_turn
-		turn to get the phase space portrait at
-
-	Returns
-	-------
-	DataFrame
-		phase space portrait
-	"""
-	particles.sort(interleave_lost_particles = True)
-
-	# if particle is lost before `at_turn` returns the turn it was lost at.
-	# Otherwise returns `at_turn`
-	at_turn_flat = list(map(lambda x: at_turn if x > at_turn else x, particles.at_turn))
-	
-	res = {}
-	
-	res['x'] = np.array(list(map(lambda x, y: x[y], monitor.x, at_turn_flat)))
-	res['px'] = np.array(list(map(lambda x, y: x[y], monitor.px, at_turn_flat)))
-	res['y'] = np.array(list(map(lambda x, y: x[y], monitor.y, at_turn_flat)))
-	res['py'] = np.array(list(map(lambda x, y: x[y], monitor.py, at_turn_flat)))
-	res['zeta'] = np.array(list(map(lambda x, y: x[y], monitor.zeta, at_turn_flat)))
-	res['delta'] = np.array(list(map(lambda x, y: x[y], monitor.delta, at_turn_flat)))
-
-	return DataFrame(res)
-
-def compute_simple_masks(particles: xt.Particles):
-	"""
-	Compute the masks for the particles compatible for the use with the output of
-	phase portrait functions.
-	"""
-	_min_offset = 1e-7
-
-	reached_start = abs(particles.s) < _min_offset
-	lost = lambda at_turn: particles.at_turn < at_turn
-
-	res = {
-		'reached_start': reached_start,
-		'lost': lost,
-		'lost_at_start': lambda at_turn: np.logical_and(lost(at_turn), reached_start),
-		'lost_not_at_start': lambda at_turn: np.logical_and(lost(at_turn), ~reached_start)
-	}
-
-	return res
-
-def find_triangle_vertices(x: np.ndarray, y: np.ndarray):
-	points = np.column_stack((x, y))
-	n_points = len(points)
-
-	# the longest side of the triangle
-	dist_max = 0
-	vertice1_index, vertice2_index = 0, 0
-	for i in range(n_points):
-		for j in range(i + 1, n_points):
-			dist = np.linalg.norm(points[i] - points[j])
-			if dist > dist_max:
-				dist_max = dist
-				vertice1_index, vertice2_index = i, j
-
-	vertice1 = points[vertice1_index]
-	vertice2 = points[vertice2_index]
-
-	def point_line_distance(x, linepoint1, linepoint2):
-		return np.abs(np.cross(linepoint2 - linepoint1, x - linepoint1)) / np.linalg.norm(linepoint2 - linepoint1)
-
-	# the furthest point from a line between
-	# vertice1 and vertice2
-	dist_max = 0
-	vertice3_index = None
-	for i in range(n_points):
-		if i == vertice1_index or i == vertice2_index:
-			continue
-		dist = point_line_distance(points[i], vertice1, vertice2)
-		if dist > dist_max:
-			dist_max = dist
-			vertice3_index = i
+	def __init__(self, 
+		xlim: list,
+		pxlim: list,
+		every: int = 1_000,
+		n_bins: int = 100,
+		in_normalised_coordinates: bool = False,
+		tw: xt.twiss.TwissTable | None = None,
+		nemitt_x: float | None = None  
+		):
+		"""
+		Parameters
+		----------
+		xlim
+			2 elements array with sampling range for `x`
+		pxlim
+			2 elements array with sampling range for `px`
+		every
+			Frequency of the phase space snapshots
+		n_bins
+			Number of bins to use in each plane
+		in_normalised_coordinates
+			If True, coordinates are assumed to be normalized coordinates. 
+			If `True` `tw` must be provided too.
+		tw
+			If using normalized coordinates, Twiss table is used to convert the coodinates
+		nemitt_x
+			Normalised horizontal emittance, when normalised coordinates are used
+		"""
+		self.x_edges = np.linspace(*xlim, n_bins + 1)
+		self.px_edges = np.linspace(*pxlim, n_bins + 1)
+		self.in_normalised_coordinates = in_normalised_coordinates
+		self.every = every
+		self.tw = tw
+		self.nemitt_x = nemitt_x
 		
-	vertice3 = points[vertice3_index]
-	
-	return np.array([vertice1, vertice2, vertice3])
+		self._call = 0
+		self.turns = []
+		self.histograms = []
+		self.n_alive = []
+		
+	def __call__(self, line: xt.Line, particles:xt.Particles, **kwargs):
+		"""
+		The function used to do the logging inside `xtrack.Line.track()`.
+		"""
+		sample_now = (self._call % self.every) == 0
+		turn = self._call
+		self._call += 1
+
+		if not sample_now:
+			return None
+		
+		ctx = particles._context
+		
+		x = ctx.nparray_from_context_array(particles.x)
+		px = ctx.nparray_from_context_array(particles.px)
+		state = ctx.nparray_from_context_array(particles.state)
+		alive = state > 0
+
+		x_for_hist = x[alive]
+		px_for_hist = px[alive]
+
+		if self.in_normalised_coordinates:
+			norm_coords = self.tw.get_normalized_coordinates(
+				line.build_particles(x = x_for_hist, px = px_for_hist), 
+				nemitt_x = self.nemitt_x, 
+				nemitt_y = 0
+			)
+			x_for_hist = norm_coords.x_norm
+			px_for_hist = norm_coords.px_norm
+		
+		hist, _, _ = np.histogram2d(
+			x_for_hist,
+			px_for_hist,
+			bins = (self.x_edges, self.px_edges),
+		)
+
+		hist = hist.astype(np.uint32)
+		alive_particles = np.sum(alive)
+		
+		self.turns.append(turn)
+		self.histograms.append(hist)
+		self.n_alive.append(alive_particles)
+
+		return alive_particles
