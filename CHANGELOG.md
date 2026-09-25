4d0efbc281e6be87b759afe403ecca1ab5fd90e8
Andrii Pastushenko <a.pastushenko@gsi.de>
2026-09-25 16:02:24 +0200

    +realign vertical mad apertures

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,11 @@
 # Changelog
 
-### [0.1.0] - 16-09-2026
+## [Unreleased]
+
+### Changed
+
+- `toolbox.realign_mad_apertures()` now realigns vertical rectangular apertures too.
+
+## [0.1.0] - 16-09-2026
+
 First version release.
--- toolbox/misc.py
+++ toolbox/misc.py
@@ -8,41 +8,64 @@ BEAMPIPE_LIMIT = 0.150
 
 def realign_mad_apertures(line: xt.Line, suppress_output = True):
 	"""
-	Realignes to non symmetric apertures that are defined in MAD-X
-	as symmetric aperture with offset.
+	Realigns to non-symmetric apertures that were defined in MAD-X
+	via a loophole (big symmetric aperture misaligned transversely).
+
+	It has a hardcoded limit `BEAMPIPE_LIMIT = 0.15` for the maximum
+	absolute aperture in either direction. The larger apertures are
+	set to a very large value (as used in `xsuite`).
 	"""
 	for name in line.element_dict:
 		element = line.element_dict[name]
-		try:
+
+		if isinstance(element, xt.LimitRect):
+
 			if element.shift_x != 0.0:
-				if isinstance(element, xt.LimitRect):
-					if not suppress_output:
-						print(f"{element.__class__.__name__}, name = '{name}', \
-							shift_x = {element.shift_x}\n\t min_x = {element.min_x}, \
-							max_x = {element.max_x}, min_y = {element.min_y}, max_y = {element.max_y}"
-						)
-					
-					# modifying the apertures wrt to the offsets
-					element.min_x += element.shift_x # lower jaw
-					element.max_x += element.shift_x # upper jaw
+				if not suppress_output:
+					print(f"{element.__class__.__name__}, name = '{name}', \
+						shift_x = {element.shift_x}\n\t min_x = {element.min_x}, \
+						max_x = {element.max_x}"
+					)
 
-					# removing 1 of the jaws when it is too larger (abs > 150 mm)
-					if abs(element.min_x) > BEAMPIPE_LIMIT:
-						element.min_x = -UNLIMITED
-					if abs(element.max_x) > BEAMPIPE_LIMIT:
-						element.max_x = UNLIMITED
+				# modifying the apertures wrt to the offsets
+				element.min_x += element.shift_x # lower jaw
+				element.max_x += element.shift_x # upper jaw
 
-					element.shift_x = 0.0
-					
-				elif isinstance(element, xt.LimitEllipse):
-					warn("The elliptic aperture is missaligned!")
-					if not suppress_output:
-						print(f"{element.__class__.__name__}, name = '{name}', \
-							shift_x = '{element.shift_x}'\n\t a = {element.a}, b = {element.b}")
-				else:
-					raise Exception("Element is not aperture element")
-		except AttributeError:
-			pass
+				# removing 1 of the jaws when it is too large (abs > 150 mm)
+				if abs(element.min_x) > BEAMPIPE_LIMIT:
+					element.min_x = -UNLIMITED
+				if abs(element.max_x) > BEAMPIPE_LIMIT:
+					element.max_x = UNLIMITED
+
+				element.shift_x = 0.0
+
+			if element.shift_y != 0.0:
+				if not suppress_output:
+					print(f"{element.__class__.__name__}, name = '{name}', \
+						shift_y = {element.shift_y}\n\t min_y = {element.min_y}, \
+						max_y = {element.max_y}"
+					)
+
+				# modifying the apertures wrt to the offsets
+				element.min_y += element.shift_y # lower jaw
+				element.max_y += element.shift_y # upper jaw
+
+				# removing 1 of the jaws when it is too large (abs > 150 mm)
+				if abs(element.min_y) > BEAMPIPE_LIMIT:
+					element.min_y = -UNLIMITED
+				if abs(element.max_y) > BEAMPIPE_LIMIT:
+					element.max_y = UNLIMITED
+
+				element.shift_y = 0.0
+
+		elif isinstance(element, xt.LimitEllipse):
+			if element.shift_x != 0.0 or element.shift_y != 0.0:
+				warn("The elliptic aperture is misaligned!")
+				if not suppress_output:
+					print(f"{element.__class__.__name__}, name = '{name}', \
+						shift_x = '{element.shift_x}', shift_y = '{element.shift_y}', \
+						\n\t a = {element.a}, b = {element.b}")
+
 
 def _remove_inactive_multipoles_fix(line: xt.Line):
 	"""
